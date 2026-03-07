d3e3ecf0b3bde774855439deb85a2f774a477b46
Gishnu Gompa <75305540+sproutcake23@users.noreply.github.com>
2026-03-07 16:30:17 +0530

    Solve Maximum subarray

---
+++ solutions/MaximumSubarray.java
@@ -0,0 +1,18 @@
+package solutions;
+
+public class MaximumSubarray {
+    public int maxSubArray(int[] nums) {
+        int sum = 0;
+        int maxsum = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (sum > maxsum) {
+                maxsum = sum;
+            }
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return maxsum;
+    }
+}
