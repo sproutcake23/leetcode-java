08d849cede1980e84353cc0df37ee32046cfdd76
Gishnu Gompa <75305540+sproutcake23@users.noreply.github.com>
2026-05-05 17:10:20 +0530

    Solve kth smallest element in a BST

---
+++ solutions/kthSmallestElementBST.java
@@ -0,0 +1,22 @@
+package solutions;
+
+public class kthSmallestElementBST {
+    private int count = 0;
+    private int ans = 0;
+
+    public int kthSmallest(TreeNode root, int k) {
+        inorder(root, k);
+        return ans;
+    }
+
+    private void inorder(TreeNode root, int k) {
+        if (root == null) return;
+        inorder(root.left, k);
+        count++;
+        if (count == k) {
+            ans = root.val;
+            return;
+        }
+        inorder(root.right, k);
+    }
+}
